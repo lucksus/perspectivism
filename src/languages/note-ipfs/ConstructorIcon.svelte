17da46e64cdd87593ec63aca6314fad310460078
Nicolas Luck <nicolas@lucksus.eu>
2020-09-27 02:01:41 +0200

    Note-ipfs constructor icon dimensions

--- src/languages/note-ipfs/ConstructorIcon.svelte
+++ src/languages/note-ipfs/ConstructorIcon.svelte
@@ -8,6 +8,22 @@
 
 </script>
 
-<input bind:value={text}>
-<button on:click={()=>commitExpression(text)}>Commit</button>
-<button on:click={discard}>Discard</button>
+<div class="container">
+    <input bind:value={text}>
+    <button on:click={()=>commitExpression(text)}>Commit</button>
+    <button on:click={discard}>Discard</button>
+</div>
+
+
+<style>
+    .container {
+        color: burlywood;
+        width: 400px;
+        height: 300px;
+    }
+
+    input {
+        width: 100%;
+        height: 200px;
+    }
+</style>
