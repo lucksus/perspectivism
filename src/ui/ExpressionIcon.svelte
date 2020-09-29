2786a1a39d38632e24401e32c70cd221ba890325
Nicolas Luck <nicolas@lucksus.eu>
2020-09-29 21:56:28 +0200

    Fixed instantiation of web components but without shadow root

--- src/ui/ExpressionIcon.svelte
+++ src/ui/ExpressionIcon.svelte
@@ -13,11 +13,23 @@
 
     let expressionRef: void | ExpressionRef = null
     let expression: void | Expression = null
+    let customElementName: void | string = null
+
     try {
         expressionRef = parseExprURL(expressionURL)
-
     } catch(e) {
         console.error(e)
+    }
+
+    function iconComponentName(languageAddress: string): string {
+        const onlyLetters = languageAddress
+            .split('')
+            .filter(letter => { return /[a-z]|[A-Z]/.test(letter)})
+            .join('')
+        const short = onlyLetters.substr(onlyLetters.length-5, 5)
+
+        console.debug(languageAddress, '->', onlyLetters, '->', short)
+        return 'icon-'+short
     }
 
     async function loadExpression() {
@@ -32,34 +44,29 @@
         loading = false
     }
 
-    async function loadIcon() {
-        console.debug("ExpressionIcon loading icon with ref:", JSON.stringify(expressionRef))
-        const code = await languageController.getIcon(expressionRef)
-        componentConstructor = iconComponentFromString(code, "icon")
-        
+    async function getComponentConstructor() {
+        componentConstructor = customElements.get(customElementName)
+        if(!componentConstructor) {
+            try {
+                console.debug("ExpressionIcon loading icon with ref:", JSON.stringify(expressionRef))
+                const code = await languageController.getIcon(expressionRef)
+                componentConstructor = iconComponentFromString(code, "icon")
+                customElements.define(customElementName, componentConstructor)
+            } catch (e) {
+                componentConstructor = customElements.get(customElementName)
+            }
+        }
     }
 
     $: if(expressionRef) {
+        customElementName = iconComponentName(expressionRef.language.address)
         loadExpression()
-        if(!componentConstructor) {
-            loadIcon()
-        }
     }
 
-    $: if(expression && componentConstructor) {
-        const container = document.getElementById("component-container")
-        container.innerHTML = ''
-
-        let component
-        try{
-            component = new componentConstructor()
-        } catch(e) {
-            console.debug("error creating component:", e)
-            customElements.define(`icon-f`, componentConstructor);
-            component = new componentConstructor()
+    $: if(customElementName) {
+        if(!componentConstructor) {
+            getComponentConstructor()
         }
-        component.expression = expression
-        container.appendChild(component)
     }
     
 
@@ -71,4 +78,8 @@
 {:else if failed}
     Loading failed!
 {/if}
-<div id="component-container"></div>
+
+{#if componentConstructor}
+    <h3>Icon:</h3>
+    <svelte:component this="{componentConstructor}" expression={expression}/>
+{/if}
