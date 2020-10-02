876aebd230de84dc7a61ccb5f52acbc3b940a4e4
Nicolas Luck <nicolas@lucksus.eu>
2020-10-03 00:09:44 +0200

    Add missing ExpressionUI files from last Language refactoring

---
+++ src/languages/note-ipfs/noteExpressionUI.ts
@@ -0,0 +1,13 @@
+import type { ExpressionUI } from "../../acai/Language";
+import Icon from './build/Icon.js'
+import ConstructorIcon from './build/ConstructorIcon.js'
+
+export class NoteExpressionUI implements ExpressionUI {
+    icon(): string {
+        return Icon
+    }
+
+    constructorIcon(): string {
+        return ConstructorIcon
+    }
+}
