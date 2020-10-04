8ee4c6c01835570b6e24d83c0497c73a2777495f
Nicolas Luck <nicolas@lucksus.eu>
2020-10-04 21:33:07 +0200

    Resilient gnu-links Settings icon

--- src/languages/gun-links/Settings.svelte
+++ src/languages/gun-links/Settings.svelte
@@ -3,9 +3,20 @@
 <script lang="ts">
     export let settings: object
     export let updateSettings
+
+    if(!settings) {
+        settings = {
+            gunDbPeer: ''
+        }
+    }
+
 </script>
 
-<div class="container">
-    <input bind:value={settings.gunDbPeer}>
-    <button on:click={()=>updateSettings(settings)}>Update</button>
+<div class="container" on:click:stopPropagation>
+    {#if settings && updateSettings}
+        <input bind:value={settings.gunDbPeer}>
+        <button on:click={()=>updateSettings(settings)}>Update</button>
+    {:else}
+        Loading...
+    {/if}
 </div>
