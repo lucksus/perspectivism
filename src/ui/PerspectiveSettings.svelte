1ccc56a8123833f64c1f96bd7ff199d55360efad
Nicolas Luck <nicolas@lucksus.eu>
2020-10-05 17:18:53 +0200

    Select links sharing Language in Perspective settings

--- src/ui/PerspectiveSettings.svelte
+++ src/ui/PerspectiveSettings.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
     export let perspectiveName
     export let perspectiveStore
+    export let languageController
 
     import FormField from '@smui/form-field';
     import Textfield from '@smui/textfield'
     import Button, {Label} from '@smui/button';
+    import Select, {Option} from '@smui/select';
     import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
 
 
     let perspective = $perspectiveStore[perspectiveName]
+    let linkLanguages = []
+    let linkLanguagesLoadingDone = false
+    
+    async function loadLinkLanguages() {
+        linkLanguages = await languageController.getLanguagesWithLinksAdapter()
+        linkLanguagesLoadingDone = true
+    }
+
+    loadLinkLanguages()
+    
 
     function save() {
         perspectiveStore.update(perspectiveName, perspective)
@@ -18,11 +30,26 @@
 </script>
 
 {#if perspective}
+<p>
     <FormField>
+        <span>Name:</span>
         <Textfield bind:value={perspective.name} label="Name" />
-        <span slot="label">Name</span>
     </FormField>
-
+</p>
+<p>
+    <FormField>
+        <span>Link sharing language:</span>
+        {#if linkLanguagesLoadingDone}
+        <Select bind:value={perspective.linksSharingLanguage} label="Sharing Language">
+            <Option value=""></Option>
+            {#each linkLanguages as lang}
+              <Option value={lang.address} selected={lang.address == perspective.linksSharingLanguage}>{lang.name}</Option>
+            {/each}
+        </Select>
+        {/if}
+    </FormField>
+</p>    
+{perspective.linksSharingLanguage}
     <Button on:click={save}>
         <Label>Ok</Label>
     </Button>
