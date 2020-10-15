dd6ee831b3c5ace9a97ea05132fc1b97556a49ff
Nicolas Luck <nicolas@lucksus.eu>
2020-10-15 15:27:17 +0200

    PerspectiveSettings table

--- src/ui/PerspectiveSettings.svelte
+++ src/ui/PerspectiveSettings.svelte
@@ -9,6 +9,8 @@
     import Button, {Label} from '@smui/button';
     import Select, {Option} from '@smui/select';
     import { createEventDispatcher } from 'svelte';
+    import DataTable, {Head, Body, Row, Cell} from '@smui/data-table';
+
     const dispatch = createEventDispatcher();
 
 
@@ -28,32 +30,50 @@
     function save() {
         dispatch('submit', perspective.uuid)
     }
+
+    function cancel() {
+        dispatch('cancel')
+    }
 </script>
 
 {#if perspective}
-<p>
-    <FormField>
-        <span>Name:</span>
-        <Textfield bind:value={perspective.name} label="Name" />
-    </FormField>
-</p>
-<p>
-    <FormField>
-        <span>Link sharing language:</span>
-        {#if linkLanguagesLoadingDone}
-        <Select bind:value={perspective.linksSharingLanguage} label="Sharing Language">
-            <Option value=""></Option>
-            {#each linkLanguages as lang}
-              <Option value={lang.address} selected={lang.address == perspective.linksSharingLanguage}>{lang.name}</Option>
-            {/each}
-        </Select>
-        {/if}
-    </FormField>
-</p>    
-{perspective.linksSharingLanguage}
-    <Button on:click={save}>
-        <Label>Ok</Label>
-    </Button>
+<div class="perspective-settings-container">
+    <h1>Perspective Settings</h1>
+    <DataTable>
+        <Body>
+            <Row>
+                <Cell>Name:</Cell>
+                <Cell><Textfield bind:value={perspective.name} label="Name" /></Cell>
+            </Row>
+            <Row>
+                <Cell>Link sharing language:</Cell>
+                <Cell>
+                    {#if linkLanguagesLoadingDone}
+                    <Select bind:value={perspective.linksSharingLanguage} label="Sharing Language">
+                        <Option value=""></Option>
+                        {#each linkLanguages as lang}
+                        <Option value={lang.address} selected={lang.address == perspective.linksSharingLanguage}>{lang.name}</Option>
+                        {/each}
+                    </Select>
+                    {/if}
+                </Cell>
+            </Row>
+        </Body>
+    </DataTable>
+</div>
+<p></p>
+<Button on:click={save}>
+    <Label>Ok</Label>
+</Button>
+<Button on:click={cancel}>
+    <Label>Cancel</Label>
+</Button>
 {:else}
     Loading...
 {/if}
+
+<style>
+    .perspective-settings-container {
+        min-width: 400px;
+    }
+</style>
