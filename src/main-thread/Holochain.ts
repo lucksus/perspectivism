592624b8a72c2fb34019388a16a066ac7e9219cf
Nicolas Luck <nicolas@lucksus.eu>
2020-12-29 20:50:56 +0100

    WIP Holochain service connecting to conductor via websockets

--- src/main-thread/Holochain.ts
+++ src/main-thread/Holochain.ts
@@ -3,6 +3,7 @@ import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
 import path from 'path'
 import { execHolochain } from '@holochain-open-dev/holochain-run-dna/src/execHolochain'
+import { installApp } from '@holochain-open-dev/holochain-run-dna/src/installApp'
 import { rootPath } from 'electron-root-path'
 export default class HolochainLanguageDelegate {
     #languageHash
@@ -14,7 +15,7 @@ export default class HolochainLanguageDelegate {
     }
 
     registerDNA(dnaFile: Buffer, nick: String) {
-        
+        this.#holochainService.ensureInstallDNAforLanguage(this.#languageHash, nick, dnaFile)
     }
 
     call(dnaNick: String, fnName: String, params: object) {
@@ -24,22 +25,49 @@ export default class HolochainLanguageDelegate {
 
 export class HolochainService {
     #cellsByLanguage: any
+    #adminPort: number
+    #adminWebsocket: AdminWebsocket
+    #appWebsocket: AppWebsocket
 
     constructor(dbAdapter, configPath) {
         this.#cellsByLanguage = low(dbAdapter)
         const holochainAdminPort = 1337
         process.env.PATH = `${rootPath}:${process.env.PATH}`
         console.debug("PATH is now:", process.env.PATH)
-        execHolochain(holochainAdminPort, configPath)
+        execHolochain(holochainAdminPort, configPath).then(async result => {
+            this.#adminPort = result[1]
+            this.#adminWebsocket = await AdminWebsocket.connect(
+                `ws://localhost:${this.#adminPort}`
+            )
+
+            console.debug("Holochain admin interface connected on port", this.#adminPort)
+
+            const appSocketPort = this.#adminPort + 1
+            this.#adminWebsocket.attachAppInterface({ port: appSocketPort })
+            this.#appWebsocket = await AppWebsocket.connect(`ws://localhost:${appSocketPort}`)
+
+            console.debug("Holochain app interface connected on port", appSocketPort)
+
+            // @ts-ignore
+            this.ensureInstallDNAforLanguage()
+        })
     }
 
-    ensureInstallDNAforLanguage() {
-
+    async ensureInstallDNAforLanguage(lang: String, nick: String, dnaFile: Buffer) {
+        const cellId = HolochainService.cellId(lang, nick)
+        const installedCellIds = await this.#adminWebsocket.listCellIds()
+        console.debug("Installed cells:", installedCellIds)
     }
 
     getDelegateForLanguage(languageHash: String) {
         return new HolochainLanguageDelegate(languageHash, this)
     }
+
+    static cellId(languageHash: String, dnaNick: String) {
+        return `${languageHash}-${dnaNick}`
+    }
+
+
 }
 
 export function init(configFilePath, dbFilePath) {
