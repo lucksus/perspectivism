2809768105e92ae05f65854c4cf7d9c78704ad23
Nicolas Luck <nicolas@lucksus.eu>
2021-01-09 18:39:34 +0100

    Forgot to commit LinkExtern previously

---
+++ src/ui/LinkExtern.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { mutation } from "svelte-apollo";
+    import { OPEN_LINK } from './graphql_queries';
+    const GQL_OPEN_LINK = mutation(OPEN_LINK)
+
+    export let url: String;
+
+    function click() {
+        GQL_OPEN_LINK({variables: { url }})
+    }
+</script>
+
+<a class="link" href="#" on:click={click}><slot></slot></a>
+
+<style>
+    .link {
+        cursor: pointer;
+    }
+</style>
