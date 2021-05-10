100f4354c1b7b410ad35076bcf76b12d9145f453
Nicolas Luck <nicolas@lucksus.eu>
2021-05-10 21:04:43 +0200

    Add get-languages build step to download pre-build languages currently only social-context

---
+++ get-languages.js
@@ -0,0 +1,14 @@
+const fs = require('fs-extra')
+const wget = require('node-wget-js')
+
+const languages = {
+    'social-context': 'ttps://github.com/juntofoundation/Social-Context/releases/download/0.0.1/bundle.js'
+}
+
+for(lang in languages) {
+    const url = languages[lang]
+    const dir = `./src/languages/${lang}/build`
+    fs.ensureDir(dir)
+    const dest = dir + '/bundle.js'
+    wget({url, dest})
+}
