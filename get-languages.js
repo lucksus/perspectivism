f0d5f55b2ac246d453744db2a382cbc5abc4c0b3
Nicolas Luck <nicolas@lucksus.eu>
2021-05-17 23:10:17 +0200

    Download social-context DNA as well

--- get-languages.js
+++ get-languages.js
@@ -2,13 +2,25 @@ const fs = require('fs-extra')
 const wget = require('node-wget-js')
 
 const languages = {
-    'social-context': 'https://github.com/juntofoundation/Social-Context/releases/download/0.0.1/bundle.js'
+    'social-context': {
+        bundle: 'https://github.com/juntofoundation/Social-Context/releases/download/0.0.1/bundle.js',
+        dna: 'https://github.com/juntofoundation/Social-Context/releases/download/0.0.1/social-context.dna'
+    }
 }
 
 for(lang in languages) {
-    const url = languages[lang]
-    const dir = `./src/languages/${lang}/build`
-    fs.ensureDir(dir)
-    const dest = dir + '/bundle.js'
+    const dir = `./src/languages/${lang}`
+    fs.ensureDir(dir + '/build')
+
+    // bundle
+    let url = languages[lang].bundle
+    let dest = dir + '/build/bundle.js'
     wget({url, dest})
+
+    // dna
+    if(languages[lang].dna) {
+        url = languages[lang].dna
+        dest = dir + `/${lang}.dna`
+        wget({url, dest})
+    }
 }
