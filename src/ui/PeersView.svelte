79989a0a088341a1448d43250ff1e9ca608ba7ea
Nicolas Luck <nicolas@lucksus.eu>
2021-05-27 23:56:48 +0200

    Make PeersView actually work

--- src/ui/PeersView.svelte
+++ src/ui/PeersView.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { query, mutation, getClient } from "svelte-apollo";
     import { gql } from '@apollo/client';
-    import { PERSPECTIVES, ADD_PERSPECTIVE, PUBLISH_PERSPECTIVE, ADD_LINK, LINKS_DATED } from './graphql_queries'
+    import { PERSPECTIVES, PERSPECTIVE, ADD_PERSPECTIVE, PUBLISH_PERSPECTIVE, ADD_LINK, LINKS_DATED } from './graphql_queries'
     import subMinutes from 'date-fns/subMinutes'
 
     const WORLD_PERSPECTIVE_NAME = '__WORLD'
@@ -22,6 +22,7 @@
     const addLink = mutation(ADD_LINK)
 
     async function ensureGlobalAppPerspective() {
+        console.log("Ensure global shared app perspective '__WORLD'")
         const result = await gqlClient.query({query: PERSPECTIVES})
         if(!result.data) {
             console.error(result)
@@ -31,6 +32,7 @@
         let found = perspectives.find( e => e.name === WORLD_PERSPECTIVE_NAME)
 
         if(!found) {
+            console.log("'__WORLD' not found locally - creating..")
             const add_result = logError(await addPerspective({
                 variables: { name: WORLD_PERSPECTIVE_NAME }
             }))
@@ -40,49 +42,77 @@
 
         perspectiveUUID = found.uuid
 
-        if(!found.sharedPerspective) {
+        console.log("'__WORLD' UUID is:", perspectiveUUID)
+
+        const perspectiveResult = logError(await gqlClient.query({
+            query: PERSPECTIVE,
+            variables: { uuid: perspectiveUUID }
+        }))
+        const perspective = perspectiveResult.data.perspective
+
+        console.log("__WORLD:", perspective)
+
+
+        if(!perspective.sharedPerspective) {
+            console.log("'__WORLD' is not shared yet - publishing perspective...")
             logError(await publishPerspective({
                 variables: {
-                    uuid: found.uuid,
-                    name: found.name,
+                    uuid: perspective.uuid,
+                    name: perspective.name,
                     description: "All Peers",
-                    type: 'holochain'
+                    type: 'holochain',
+                    uid: 'Perspect3ve__WORLD',
+                    requiredExpressionLanguages: [],
+                    allowedExpressionLanguages: []
                 }
             }))
+
+            console.log("'__WORLD' was just published")
+        } else {
+            console.log("'__WORLD' was published before")
         }
     }
 
     async function getOnlinePeers() {
         const untilDate = Date.now()
         const fromDate = subMinutes(untilDate, 2)
+        const variables = {
+            perspectiveUUID,
+            fromDate,
+            untilDate,
+        }
+
+        console.log("PeersView getting online peers with LINKS_DATED query with variables:", variables)
         const result = logError(await gqlClient.query({ 
             query: LINKS_DATED,
-            variables: {
-                perspectiveUUID,
-                fromDate,
-                untilDate,
-            }
+            variables
         }))
 
         onlinePeers = result.data.links.map(l => l.author.did)
     }
 
     async function publishOnlineStatus() {
+        console.log("PeersView publishing online status...")
         const newLink = {
             source: 'status',
             target: 'online',
         }
         logError(await addLink({
             variables: {
+                perspectiveUUID,
                 link: JSON.stringify(newLink)
             }
         }))
     }
 
     async function init() {
+        console.log("Initializing PeersView...")
         await ensureGlobalAppPerspective()
+        console.log("Initializing PeersView 1")
         await publishOnlineStatus()
+        console.log("Initializing PeersView 2")
         await getOnlinePeers()
+        console.log("Initializing PeersView 3")
         setInterval(getOnlinePeers, 5000)
     }
 
