ca8ba6134bc54b0d983778f8f292deae56f03f07
Nicolas Luck <nicolas@lucksus.eu>
2021-12-14 12:21:24 +0100

    WIP: code to render perspectives in zoom root (can't get svelte context)

--- src/ui/ZoomRoot.svelte
+++ src/ui/ZoomRoot.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
-    
-    console.log("hi from zoom root")
-
+    import { getContext } from "svelte";
+    import type { Ad4mClient } from '@perspect3vism/ad4m'
+    import { perspectivesStore } from "./PerspectivesStore";
+    const ad4m: Ad4mClient = getContext('ad4mClient')
+    console.log("ad4m in zoomroot:", ad4m)
+    let perspectives = perspectivesStore(ad4m)
 </script>
 
 <div class="z-view">
-    <h1>Zoom Root</h1>
+    <div id="perspectives">
+        <h1>Perspectives</h1>
+        <ul>
+            {#each $perspectives as perspective}
+            <li class="perspective-icon zoom-me" data-to="Perspective">
+                <span class="perspective-name">
+                    {perspective.name}
+                </span>
+            </li>
+            {/each}
+        </ul>
+    </div>
+    
+    <div class="zoom-me" data-to="stringView">zoom me</div>
 </div>
