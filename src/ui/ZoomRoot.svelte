b5afb7f1b15026fc618507583e4e017af4baf7df
Nicolas Luck <nicolas@lucksus.eu>
2021-12-16 19:24:09 +0100

    ZoomRoot bubbles

--- src/ui/ZoomRoot.svelte
+++ src/ui/ZoomRoot.svelte
@@ -5,19 +5,113 @@
     const ad4m: Ad4mClient = getContext('ad4mClient')
     console.log("ad4m in zoomroot:", ad4m)
     let perspectives = perspectivesStore(ad4m)
+    console.log($perspectives)
+    let agentPerspective 
+    $: agentPerspective = $perspectives.find(e => e.name == '__agent_public_perspective')
+
+    async function createNewPerspective() {
+		let number = 1
+		let prefix = "New Perspective "
+		while($perspectives.includes(prefix+number)) {
+			number++
+		}
+
+		const name = prefix+number
+		const result = await ad4m.perspective.add(name)
+		console.log("Perspective added:", result)
+	}
 </script>
 
-<div id="perspectives">
-    <h1>Perspectives</h1>
-    <ul>
-        {#each $perspectives as perspective}
-        <li class="perspective-icon">
-            <span class="perspective-name zoom-me" data-to="PerspectiveWrapper" data-uuid={perspective.uuid}>
-                {perspective.name}
-            </span>
-        </li>
-        {/each}
-    </ul>
+<div class="perspectives">
+    <div class="public">
+        <h2>Public Perspectives</h2>
+        {#if agentPerspective}
+            <div class="perspective-box agent-perspective zoom-me" data-to="PerspectiveWrapper" data-uuid={agentPerspective.uuid}>
+                Public Agent Perspective
+            </div>
+        {/if}
+
+        <h2>Neighbourhoods</h2>
+        <ul class="perspective-list neighbourhoods">
+            {#each $perspectives as perspective}
+                {#if perspective !== agentPerspective && perspective.sharedUrl}
+                    <li class="perspective-icon">
+                        <div class="perspective-box zoom-me" data-to="PerspectiveWrapper" data-uuid={perspective.uuid}>
+                            {perspective.name}
+                        </div>
+                    </li>
+                {/if}
+            {/each}
+        </ul>
+    </div>
+
+    <div class="private">
+        <h2>Private Perspectives <div class="perspective-box plus-button" on:click={()=>createNewPerspective()}>+</div></h2>
+        <ul class="perspective-list private-perspectives">
+            {#each $perspectives as perspective}
+                {#if perspective !== agentPerspective && !perspective.sharedUrl}
+                    <li class="perspective-icon">
+                        <div class="perspective-box zoom-me" data-to="PerspectiveWrapper" data-uuid={perspective.uuid}>
+                            {perspective.name}
+                        </div>
+                    </li>
+                {/if}
+            {/each}
+        </ul>
+        
+    </div>
+
 </div>
 
-<div class="zoom-me" data-to="stringView">zoom me</div>
+<style>
+    .perspectives {
+        padding: 20px;
+        text-align: center;
+    }
+    .perspective-box {
+        display: inline-block;
+        width: 200px;
+        height: 160px;
+        margin: auto;
+        border-radius: 50px;
+        background-color: aqua;
+        text-align: center;
+        line-height: 160px;
+
+    }
+
+    .agent-perspective {
+        background-color: red;
+    }
+
+    .perspective-list {
+        display: inline-block;
+        list-style-type: none;
+        padding: 0;
+    }
+
+    .perspective-icon {
+        display: inline-block;
+    }
+
+    .private {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 33%;
+        background-color: seagreen;
+        overflow: scroll;
+    }
+
+    .plus-button {
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 50px;
+        height: 40px;
+        font-size: 30px;
+        line-height: normal;
+        cursor: pointer;
+    }
+</style>
