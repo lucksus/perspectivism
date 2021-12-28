1bad97be584ddeb35e6d771874630cdf185d71cc
Nicolas Luck <nicolas@lucksus.eu>
2021-12-28 23:07:24 +0100

    Add toggling of prolog zomes

--- src/ui/PrologExpressionView.svelte
+++ src/ui/PrologExpressionView.svelte
@@ -26,16 +26,47 @@
             predicate: 'ad4m://has_zome'
         }))
 
-        existingZomes = links.map(link => {
-            return {
-                link,
-                code: Literal.fromUrl(link.data.target).get()
-            }
-        })
+        let inactiveLinks = await perspective.get(new LinkQuery({
+            source: 'self',
+            predicate: 'ad4m://has_inactive_zome'
+        }))
+
+        existingZomes = [
+            ...links.map(link => {
+                return {
+                    link,
+                    code: Literal.fromUrl(link.data.target).get(),
+                    active: true
+                }
+            }),
+            ...inactiveLinks.map(link => {
+                return {
+                    link,
+                    code: Literal.fromUrl(link.data.target).get(),
+                    active: false
+                }
+            })
+        ]
     }
 
     function deleteZome(zome) {
         perspective.remove(zome.link)
+    }
+
+    function makeInactiveZome(zome) {
+        perspective.remove(zome.link)
+        perspective.add(new Link({
+            ...zome.link.data,
+            predicate: 'ad4m://has_inactive_zome'
+        }))
+    }
+
+    function makeActiveZome(zome) {
+        perspective.remove(zome.link)
+        perspective.add(new Link({
+            ...zome.link.data,
+            predicate: 'ad4m://has_zome'
+        }))
     }
 
     perspective.addListener(() => {
@@ -55,7 +86,7 @@
 {#each existingZomes as zome}
 <Card variant="outlined">
     <Content>
-        <div class="zome-code">
+        <div class="zome-code" class:inactive={!zome.active}>
             {@html hljs.highlight(zome.code, {language: 'prolog'}).value}
         </div>
     </Content>
@@ -65,6 +96,18 @@
                 <ButtonIcon class="material-icons">delete</ButtonIcon>
                 <Label>Delete</Label>
             </Button>
+
+            {#if zome.active}
+                <Button variant="outlined" on:click={()=>makeInactiveZome(zome)}>
+                    <ButtonIcon class="material-icons">toggle_off</ButtonIcon>
+                    <Label>Deactivate</Label>
+                </Button>
+            {:else}
+                <Button variant="outlined" on:click={()=>makeActiveZome(zome)}>
+                    <ButtonIcon class="material-icons">toggle_on</ButtonIcon>
+                    <Label>Activate</Label>
+                </Button>
+            {/if}
         </ActionButtons>
     </Actions>
 </Card>
@@ -88,4 +131,8 @@
         overflow: scroll;
         white-space: pre;
     }
+
+    .inactive {
+        opacity: 0.4;
+    } 
 </style>
