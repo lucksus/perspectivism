1d4e975b3c84c51b50f103d6a41c875b678876b4
Nicolas Luck <nicolas@lucksus.eu>
2022-01-06 20:37:39 +0100

    Dashboard with Vis graph

--- src/ui/ZoomRoot.svelte
+++ src/ui/ZoomRoot.svelte
@@ -1,11 +1,198 @@
 <script lang="ts">
     import { getContext } from "svelte";
-    import type { Ad4mClient } from '@perspect3vism/ad4m'
+    import { Ad4mClient, Literal } from '@perspect3vism/ad4m'
     import { perspectivesStore } from "./PerspectivesStore";
+    import { Network } from 'vis-network/esnext'
+    import { DataSet } from 'vis-data'
+    import md5 from 'md5'
+
+    let agentPerspective
+    let neighbourhoods = []
+    let privatePerspectives = []
+
+    let container
+    let nodes = []
+    let nodesDataSet = new DataSet([])
+    let nodePositions = {}
+    let scale = 1
+    let network
+
+    $: if(container) initGraph()
+
+    async function initGraph() {
+        await createNetwork(container)
+        update($perspectives)
+    }
+        
+
     const ad4m: Ad4mClient = getContext('ad4mClient')
     let perspectives = perspectivesStore(ad4m)
-    let agentPerspective 
-    $: agentPerspective = $perspectives.find(e => e.name == '__agent_public_perspective')
+    perspectives.subscribe(update)
+
+    async function update(newPerspectives) {
+        for(let p of newPerspectives) {
+            console.log("procssing", p)
+            if(p.name == '__agent_public_perspective')
+                agentPerspective = p
+            else {
+                if(p.sharedUrl) {
+                    neighbourhoods = [...neighbourhoods, p]
+                } else {
+                    privatePerspectives = [...privatePerspectives, p]
+                }
+            }
+        }
+
+        nodes = await createNodesFromPerspectives()
+        nodesDataSet.clear()
+        for(let node of nodes) {
+            try{
+                nodesDataSet.add(node)
+            } catch(e) {
+
+            }
+        }
+    }
+
+    async function createNodesFromPerspectives() {
+        let nodes = []
+        
+        if(agentPerspective) {
+            let email
+            try {
+                let { did } = await ad4m.agent.me()
+                email = Literal.fromUrl(await agentPerspective.getSingleTarget({source: did, predicate: 'foaf://mbox'})).get()
+            }catch(e) {
+                console.error(e)
+            }
+
+            console.log("got email: ", email)
+            let image
+            if(email)
+                image = `http://www.gravatar.com/avatar/${md5(email)}?s=360`
+
+
+            console.log("imgae:", image)
+
+            nodes.push({
+                id: agentPerspective.uuid,
+                label: "Agent Public Perspective",
+                widthConstraint: 150,
+                shape: image ? 'image' : 'box',
+                x: 0,
+                y: 0,
+                image
+            })
+        }
+
+        let all = privatePerspectives.length
+        let step = 150
+        let left = all/2 * -step
+        for(let i=0; i < all; i++) {
+            const p = privatePerspectives[i]
+            nodes.push({
+                id: p.uuid,
+                label: p.name,
+                widthConstraint: 150,
+                shape: 'image',
+                image: 'images/silvereyeflower.png',
+                x: left + (i*step),
+                y: 200
+            })
+        }
+     
+        
+        all = neighbourhoods.length
+        step = 150
+        left = all/2 * -step
+        for(let i=0; i < all; i++) {
+            const p = neighbourhoods[i]
+            nodes.push({
+                id: p.uuid,
+                label: p.name,
+                widthConstraint: 150,
+                shape: 'image',
+                image: 'images/silvereyeflower.png',
+                x: left + (i*step),
+                y: -200
+            })
+        }
+
+        return nodes
+    }
+
+    async function createNetwork(container) {        
+        network = new Network(container, {nodes: nodesDataSet, edges: []}, {
+            nodes: {
+                borderWidth: 1,
+                shape: 'box',
+                color: 'teal',
+                font: {
+                    color: '#ffffff'
+                }
+            },
+            edges: {
+                color: 'black',
+                arrows: {
+                    to: {
+                        enabled: true,
+                    }
+                },
+                arrowStrikethrough: false,
+                smooth: false,
+            },
+            physics: {
+                enabled: false
+            }
+        })
+
+        for(let event of ['dragging', 'stabilizationProgress', 'stabilized', 'resize', 'afterDrawing']) {
+            network.on(event, () => {
+                getNodePositions()
+            })
+        }
+
+        network.on('zoom', (params) => {
+            scale = params.scale
+            getNodePositions()
+        })
+
+        network.on('oncontext', params => {
+            /*
+            if(params.nodes.length > 0) {
+                let url = params.nodes[0]
+                let pos = params.pointer.DOM
+                expressionContextMenu.open(pos.x, pos.y, url)
+            } else if(params.edges.length > 0) {
+                let edgeId = params.edges[0]
+                //@ts-ignore
+                let { from, to, label, link } = graph.edges.find(e=> e.id===edgeId)
+                console.log('oncontext link:', link)
+                let pos = params.pointer.DOM
+                if(link)
+                    linkContextMenu.open(pos.x, pos.y, link)
+            }
+            */
+        })
+
+        network.on('click', params => {
+        })
+
+        getNodePositions()
+    }
+
+    function getNodePositions() {
+        nodePositions = {}
+        for(let node of nodes) {
+            try{
+                const graphPos = network.getPosition(node.id)
+                nodePositions[node.id]=network.canvasToDOM(graphPos)
+            }catch(e){
+                console.debug("Error in getNodePositions()", e)
+            }
+        }
+    }
+
 
     async function createNewPerspective() {
 		let number = 1
@@ -21,72 +208,59 @@
 </script>
 
 <div class="perspectives">
-    <div class="public">
-        <h2>Public Perspectives</h2>
-        {#if agentPerspective}
-            <div class="perspective-box agent-perspective zoom-me" data-to="PerspectiveWrapper" 
-                data-uuid={agentPerspective.uuid}
-                data-settings={false}
-                data-agentprofile={true}
-            >
-                Public Agent Perspective
-            </div>
-        {/if}
-
-        <h2>Neighbourhoods</h2>
-        <ul class="perspective-list neighbourhoods">
-            {#each $perspectives as perspective}
-                {#if perspective !== agentPerspective && perspective.sharedUrl}
-                    <li class="perspective-icon">
-                        <div class="perspective-box zoom-me" data-to="PerspectiveWrapper" 
-                            data-uuid={perspective.uuid}
-                            data-settings=true
-                        >
-                            {perspective.name}
-                        </div>
-                    </li>
-                {/if}
-            {/each}
-        </ul>
+    <div class="graph" bind:this={container}>
     </div>
 
-    <div class="private">
-        <h2>Private Perspectives <div class="perspective-box plus-button" on:click={()=>createNewPerspective()}>+</div></h2>
-        <ul class="perspective-list private-perspectives">
-            {#each $perspectives as perspective}
-                {#if perspective !== agentPerspective && !perspective.sharedUrl}
-                    <li class="perspective-list-item">
-                        <div class="perspective-box zoom-me" data-to="PerspectiveWrapper" 
-                            data-uuid={perspective.uuid}
-                            data-settings=true
-                        >
-                            <img class="perspective-icon zoom-me" 
-                                src="images/silvereyeflower.png" 
-                                alt="perspective icon"
-                                data-to="PerspectiveWrapper" 
-                                data-uuid={perspective.uuid}
-                                data-settings=true
-                            >
-                            <span class="perspective-name zoom-me"
-                                data-to="PerspectiveWrapper" 
-                                data-uuid={perspective.uuid}
-                                data-settings=true
-                            >{perspective.name}</span>
-                        </div>
-                    </li>
+    {#each $perspectives as p}
+        <div class="perspective-icon-wrapper" style={
+            `top: ${nodePositions[p.uuid]?.y-15-(30*scale)}px; 
+            left: ${nodePositions[p.uuid]?.x-15+(50*scale)}px;
+            transform: scale(${scale*0.8});
+            `}>
+                {#if p.uuid === agentPerspective.uuid}
+                    <div class="zoom-handle material-icons zoom-me" data-to="PerspectiveWrapper" 
+                        data-uuid={p.uuid}
+                        data-settings=false
+                        data-agentprofile=true
+                    >fullscreen</div>
+                {:else}
+                    <div class="zoom-handle material-icons zoom-me" data-to="PerspectiveWrapper" 
+                        data-uuid={p.uuid}
+                        data-settings=true
+                    >fullscreen</div>
                 {/if}
-            {/each}
-        </ul>
-        
-    </div>
+        </div>
+    {/each}
 
+    <h2><div class="perspective-box plus-button" on:click={()=>createNewPerspective()}>+</div></h2>
 </div>
 
 <style>
     .perspectives {
-        padding: 20px;
-        text-align: center;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
     }
+
+    .graph {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    } 
+
+    .perspective-icon-wrapper {
+        position: absolute;
+    }
+    
+    .zoom-handle {
+        font-size: 30px;
+        background-color: #00ffff73;
+    }
+
     .perspective-box {
         position: relative;
         display: inline-block;
