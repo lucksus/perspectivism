ad7e237e5b1c035311852ae9160edeeb43518d8b
Nicolas Luck <nicolas@lucksus.eu>
2022-07-29 00:35:23 +0200

    Clicking of ontology classes in palette creates expression with tag

--- src/ui/ConstructionPalette.svelte
+++ src/ui/ConstructionPalette.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     import type { PerspectiveProxy } from '@perspect3vism/ad4m';
-
-    import List, {Item, Text, Graphic} from '@smui/list';
+    import Dialog, { Title, Content, Actions } from '@smui/dialog';
+    import ExpressionConstructorWizard from './ExpressionConstructorWizard.svelte';
+    import Button, { Label, Icon as ButtonIcon } from '@smui/button';
     import { createEventDispatcher } from 'svelte';
+    import { executeCustomAction } from "./executeCustomAction";
+
     const dispatch = createEventDispatcher();
 
     export let perspective: PerspectiveProxy
+
+    let expressionWizard
+    let expressionWizardDialog
+    let classBeingInstantiated
 
     interface OntologyClass {
         Name: string,
@@ -15,14 +22,34 @@
 
     let ontologyClasses: OntologyClass[] = []
 
-    //$: if(perspective) {
-    //    console.log("PAlette perspecive")
-        perspective.infer("register_ontology_class(Name, c), instantiate_class(c, Action), class_color(c, Color)").then(results => {
-            ontologyClasses = results
-            console.log(ontologyClasses)
-        })
-    //}
+    perspective.infer("register_ontology_class(Name, c), instantiate_class(c, Action), class_color(c, Color)").then(results => {
+        ontologyClasses = results
+        console.log(ontologyClasses)
+    })
+
+    function classClicked(name: string) {
+        classBeingInstantiated = name
+        expressionWizard.reset()
+        expressionWizardDialog.open()
+    }
+
+    async function expressionCreated(e) {
+        let url = e.detail
+        let ontologyClass = ontologyClasses.find(c => c.Name == classBeingInstantiated)
+        if(!ontologyClass) {
+            console.error("Could not find ontology class!")
+            return
+        }
+
+        await perspective.add({source: 'ad4m://self', target: url})
+        console.debug(ontologyClass.Action)
+        let action = eval(ontologyClass.Action)
+        await executeCustomAction(action, url, perspective)
         
+        expressionWizardDialog.close()
+    }
+        
+
 
 </script>
 
@@ -31,13 +58,32 @@
         <span class="material-icons">text_fields</span>
     </div>
     {#each ontologyClasses as customClass}
-        <div class="palette-item">
+        <div class="palette-item" on:click={()=>classClicked(customClass.Name)}>
             <span title="{customClass.Name}" class="ontology-class" style={`background-color: ${customClass.Color};`}>
                 <span class="ontology-class-text">{customClass.Name}</span>
             </span>
         </div>
     {/each}
 </div>
+
+<div class="dialog-class">
+    <Dialog bind:this={expressionWizardDialog}>
+        <Title>Create Expression as base for {classBeingInstantiated}</Title>
+        <Content>
+            <i>Select language of base experssion:</i>
+            <ExpressionConstructorWizard
+                bind:this={expressionWizard}
+                on:expression-created={expressionCreated}>
+            </ExpressionConstructorWizard>
+        </Content>
+        <Actions>
+            <Button on:click={expressionWizardDialog.close}>
+                <Label>Cancel</Label>
+            </Button>
+        </Actions>
+    </Dialog>    
+</div>
+
 
 <style>
    .palette-container {
@@ -66,4 +112,10 @@
         transform: translate(-50%, -50%);
         font-size: 10px;
    }
+
+   .dialog-class {
+    position: fixed;
+    width: 444px;
+    height: 419px;
+   } 
 </style>
